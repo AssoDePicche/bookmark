2bdf0c83d547bf319803f82029cb238b92ea55fd
AssoDePicche <samuelprado730@gmail.com>
2025-10-20 18:36:35 -0300

    feat: BookService.java

---
+++ backend/src/main/java/com/bookmark/application/BookService.java
@@ -0,0 +1,13 @@
+package com.bookmark.application;
+
+import com.bookmark.infrastructure.persistence.JpaBookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+  private final JpaBookRepository repository;
+
+  public BookService(JpaBookRepository repository) {
+    this.repository = repository;
+  }
+}
