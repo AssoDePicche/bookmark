14c420129ed32bbd26a9cc7f519c74a06fd5eb37
AssoDePicche <samuelprado730@gmail.com>
2025-10-26 21:42:26 -0300

    feat: add configuration annotation to open api configuration

--- backend/src/main/java/com/bookmark/infrastructure/configuration/OpenAPIConfiguration.java
+++ backend/src/main/java/com/bookmark/infrastructure/configuration/OpenAPIConfiguration.java
@@ -2,6 +2,8 @@ package com.bookmark.infrastructure.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(info = @Info(description = "OpenAPI Docs", title = "OpenAPI Specs"))
 public class OpenAPIConfiguration {}
