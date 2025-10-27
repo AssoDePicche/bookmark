e1ba04cfb4d38ffbb7006294b45be02abba169ba
AssoDePicche <samuelprado730@gmail.com>
2025-10-26 23:46:46 -0300

    refactor: decouple jpa user repository from custom user details service

--- backend/src/main/java/com/bookmark/application/CustomUserDetailsService.java
+++ backend/src/main/java/com/bookmark/application/CustomUserDetailsService.java
@@ -1,7 +1,8 @@
 package com.bookmark.application;
 
 import com.bookmark.domain.User;
-import com.bookmark.infrastructure.persistence.JpaUserRepository;
+import com.bookmark.domain.UserRepository;
+import com.bookmark.domain.Username;
 import com.bookmark.infrastructure.security.CustomUserDetails;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,15 +11,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-  private final JpaUserRepository repository;
+  private final UserRepository repository;
 
-  public CustomUserDetailsService(JpaUserRepository repository) {
+  public CustomUserDetailsService(UserRepository repository) {
     this.repository = repository;
   }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = repository.findByUsername_Value(username).orElseThrow(() -> {
+    User user = repository.findByUsername(new Username(username)).orElseThrow(() -> {
       var message = String.format("User Not Found With Username '%s'", username);
 
       throw new UsernameNotFoundException(message);
