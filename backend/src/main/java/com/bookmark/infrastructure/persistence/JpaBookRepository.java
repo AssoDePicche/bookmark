bd0308f207efa26fb0134fb4a6d0468852ddb261
AssoDePicche <samuelprado730@gmail.com>
2025-10-26 22:20:46 -0300

    refactor: remove unused imports and methods from jpa book repository

--- backend/src/main/java/com/bookmark/infrastructure/persistence/JpaBookRepository.java
+++ backend/src/main/java/com/bookmark/infrastructure/persistence/JpaBookRepository.java
@@ -1,12 +1,11 @@
 package com.bookmark.infrastructure.persistence;
 
 import com.bookmark.domain.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaBookRepository extends JpaRepository<Book, Long> {
-  Page<Book> findAll(Pageable pageable);
+  Optional<Book> findByTitle_Value(String title);
 }
