186c1919acd55adaa45abfbd28444572d273793b
AssoDePicche <samuelprado730@gmail.com>
2025-11-01 22:37:09 -0300

    test: add architecture tests

--- backend/src/test/java/com/bookmark/BookmarkApplicationTests.java
+++ backend/src/test/java/com/bookmark/BookmarkApplicationTests.java
@@ -1,10 +1,56 @@
 package com.bookmark;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@AnalyzeClasses(packages = "com.bookmark", importOptions = {ImportOption.DoNotIncludeTests.class})
 @SpringBootTest
-class BookmarkApplicationTests {
+public class BookmarkApplicationTests {
+  @ArchTest
+  public static final ArchRule domainShouldNotUseFrameworkAnnotations =
+      classes()
+          .that()
+          .resideInAPackage("..domain..")
+          .should()
+          .notBeAnnotatedWith("org.springframework.stereotype.Service")
+          .andShould()
+          .notBeAnnotatedWith("org.springframework.stereotype.Repository")
+          .andShould()
+          .notBeAnnotatedWith("org.springframework.transaction.annotation.Transactional")
+          .andShould()
+          .notBeAnnotatedWith("javax.persistence..")
+          .andShould()
+          .notBeAnnotatedWith("jakarta.persistence..");
+
+  @ArchTest
+  public static final ArchRule repositoryImplementationsMustBeInInfrastructure =
+      classes()
+          .that()
+          .haveNameMatching(".*RepositoryAdapter")
+          .or()
+          .haveNameMatching(".*Entity")
+          .should()
+          .resideInAPackage("..infrastructure.adapters.persistence..");
+
+  @ArchTest
+  public static final ArchRule controllersMustResideInInterfaces =
+      classes()
+          .that()
+          .haveNameMatching(".*Controller")
+          .should()
+          .resideInAPackage("..interfaces.controllers..")
+          .andShould()
+          .beAnnotatedWith("org.springframework.web.bind.annotation.RestController")
+          .as("Controllers should be placed in the interfaces layer and use the appropriate Spring "
+              + "annotation.");
+
   @Test
-  void contextLoads() {}
+  public void contextLoads() {}
 }
