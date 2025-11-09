5910e42dc7fbb287ccce8fd3e2005f9fc48c14ef
AssoDePicche <samuelprado730@gmail.com>
2025-11-09 00:33:38 -0300

    feat: add from string factory method to book list visibility enum

--- backend/src/main/java/com/bookmark/booklist/domain/BookListVisibility.java
+++ backend/src/main/java/com/bookmark/booklist/domain/BookListVisibility.java
@@ -3,5 +3,14 @@ package com.bookmark.booklist.domain;
 public enum BookListVisibility {
   FRIENDS,
   PRIVATE,
-  PUBLIC,
+  PUBLIC;
+
+  public static BookListVisibility from(String visibility) {
+    return switch (visibility.toLowerCase()) {
+      case "friends" -> FRIENDS;
+      case "private" -> PRIVATE;
+      case "public" -> PUBLIC;
+      default -> throw new IllegalArgumentException(String.format("'%s' Is Not A Valid Book List Visibility", visibility));
+    };
+  }
 }
