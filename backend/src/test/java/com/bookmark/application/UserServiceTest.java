28eb7e95d928a2dde8a249fcfa4252ba86ddbee3
AssoDePicche <samuelprado730@gmail.com>
2025-11-17 00:57:15 -0300

    test: update api interfaces + rm user controller test

--- backend/src/test/java/com/bookmark/application/UserServiceTest.java
+++ backend/src/test/java/com/bookmark/application/UserServiceTest.java
@@ -10,8 +10,8 @@ import com.bookmark.user.domain.Email;
 import com.bookmark.user.domain.User;
 import com.bookmark.user.domain.UserId;
 import com.bookmark.user.domain.UserRepository;
-import com.bookmark.user.domain.Username;
 import java.util.Optional;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,15 +29,12 @@ public class UserServiceTest {
   @InjectMocks private UserServiceImpl userService;
 
   private static final UserId USER_ID = new UserId();
-  private static final String USERNAME = "testuser";
   private static final String EMAIL = "test@example.com";
-  private static final String RAW_PASSWORD = "rawPass123";
-  private static final String ENCODED_PASSWORD = "$2a$10$encodedHash";
-  private static final String ROLE = "USER";
+  private static final Set<String> ROLES = Set.of("user");
   private User mockUser;
 
   private User createMockUser() {
-    return new User(USER_ID, EMAIL, USERNAME, RAW_PASSWORD, ROLE);
+    return new User(USER_ID.toString(), EMAIL, ROLES);
   }
 
   @BeforeEach
@@ -53,8 +50,6 @@ public class UserServiceTest {
 
     assertNotNull(result);
 
-    assertEquals(USERNAME, result.getUsername().toString());
-
     verify(repository).findById(USER_ID);
   }
 
@@ -68,56 +63,8 @@ public class UserServiceTest {
   }
 
   @Test
-  void queryByUsernameShouldReturnUserWhenFound() {
-    when(repository.findByUsername(new Username(USERNAME))).thenReturn(Optional.of(mockUser));
-
-    User result = userService.query(new Username(USERNAME));
-
-    assertNotNull(result);
-
-    assertEquals(EMAIL, result.getEmail().toString());
-
-    verify(repository).findByUsername(new Username(USERNAME));
-  }
-
-  @Test
-  void queryByUsernameShouldThrowNotFoundExceptionWhenNotFound() {
-    when(repository.findByUsername(new Username(USERNAME))).thenReturn(Optional.empty());
-
-    assertThrows(NotFoundException.class, () -> userService.query(new Username(USERNAME)));
-
-    verify(repository).findByUsername(new Username(USERNAME));
-  }
-
-  @Test
-  void saveShouldEncodePasswordAndSaveUserWhenUserIsNew() {
-    User newUser = createMockUser();
-    when(repository.findByUsername(new Username(USERNAME))).thenReturn(Optional.empty());
-    when(repository.findByEmail(new Email(EMAIL))).thenReturn(Optional.empty());
-    when(encoder.encode(RAW_PASSWORD)).thenReturn(ENCODED_PASSWORD);
-    when(repository.save(any(User.class))).thenReturn(newUser);
-    User savedUser = userService.save(newUser);
-    assertNotNull(savedUser);
-    assertEquals(
-        ENCODED_PASSWORD, savedUser.getPassword().toString(), "Password should be encoded");
-    verify(encoder).encode(RAW_PASSWORD.toString());
-    verify(repository).save(newUser);
-  }
-
-  @Test
-  void saveShouldThrowDuplicateEntryExceptionWhenUsernameExists() {
-    User newUser = createMockUser();
-    when(repository.findByUsername(new Username(USERNAME))).thenReturn(Optional.of(mockUser));
-    assertThrows(DuplicateEntryException.class, () -> userService.save(newUser));
-    verify(repository, never()).findByEmail(any());
-    verify(encoder, never()).encode(any());
-    verify(repository, never()).save(any());
-  }
-
-  @Test
   void saveShouldThrowDuplicateEntryExceptionWhenEmailExists() {
     User newUser = createMockUser();
-    when(repository.findByUsername(new Username(USERNAME))).thenReturn(Optional.empty());
     when(repository.findByEmail(new Email(EMAIL))).thenReturn(Optional.of(mockUser));
     assertThrows(DuplicateEntryException.class, () -> userService.save(newUser));
     verify(encoder, never()).encode(any());
