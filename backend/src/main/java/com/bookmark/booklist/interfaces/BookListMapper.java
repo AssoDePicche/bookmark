fdc373de4a4afd41ddb4902e682b14e040056fe9
AssoDePicche <samuelprado730@gmail.com>
2025-11-20 18:49:47 -0300

    refactor: remove unused imports

--- backend/src/main/java/com/bookmark/booklist/interfaces/BookListMapper.java
+++ backend/src/main/java/com/bookmark/booklist/interfaces/BookListMapper.java
@@ -3,9 +3,7 @@ package com.bookmark.booklist.interfaces;
 import com.bookmark.booklist.domain.BookList;
 import com.bookmark.common.domain.Paged;
 import com.bookmark.common.infrastructure.mapper.PageMapper;
-import org.springframework.stereotype.Component;
 
-@Component
 public class BookListMapper {
   public static BookListResponse map(BookList bookList) {
     return new BookListResponse(
