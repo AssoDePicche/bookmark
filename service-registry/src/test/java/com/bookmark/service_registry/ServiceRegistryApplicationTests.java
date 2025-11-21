483d19314372fe70cc1899c4e218d9af0a203417
AssoDePicche <samuelprado730@gmail.com>
2025-11-21 01:52:29 -0300

    feat: add service registry

---
+++ service-registry/src/test/java/com/bookmark/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,10 @@
+package com.bookmark.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+  @Test
+  void contextLoads() {}
+}
